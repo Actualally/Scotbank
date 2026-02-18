09b2d65230bf66cd85d557afea6119dfd7dd51ab
JamFred24 <jamese240624@gmail.com>
2026-02-18 16:33:07 +0000

    Deposit function added

--- src/main/java/uk/co/asepstrath/bank/Account.java
+++ src/main/java/uk/co/asepstrath/bank/Account.java
@@ -1,14 +1,22 @@
 package uk.co.asepstrath.bank;
 
 public class Account {
+    int balance;
+
+    public Account(){
+        balance = 0;
+    }
 
 
     public void deposit(int amount) {
-
+        if (amount <=0){
+            throw new IllegalArgumentException("Deposit amount must be positive");
+        }
+        balance += amount;
     }
 
     public int getBalance() {
-        return 0;
+        return balance;
     }
 
 }
--- src/test/java/uk/co/asepstrath/bank/AccountTests.java
+++ src/test/java/uk/co/asepstrath/bank/AccountTests.java
@@ -4,11 +4,35 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AccountTests {
+    Account account = new Account();
 
     @Test
     public void createAccount(){
         Account a = new Account();
         Assertions.assertTrue(a != null);
+    }
+
+    @Test
+    public void initialBalanceIsZero(){
+        Assertions.assertEquals(0,account.getBalance());
+    }
+
+    @Test
+    public void depositPositiveAmount(){
+        account.deposit(10);
+        Assertions.assertEquals(10,account.getBalance());
+    }
+
+    @Test
+    public void depositZeroThrowsException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            account.deposit(0);
+        });
+    }
+
+    @Test
+    public void depositNegativeAmountThrowsException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> account.deposit(-10));
     }
 
 
