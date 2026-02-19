0c05d1cba8dd14ae526ac40bf272439bc3edd4b9
Alexander Marshall <alexander.marshall.2024@uni.strath.ac.uk>
2026-02-19 18:22:05 +0000

    developed upon the Account class, amending or adding methods that will be required for both depositing and withdrawing. withdrawls are not functioning yet.

--- src/main/java/uk/co/asepstrath/bank/Account.java
+++ src/main/java/uk/co/asepstrath/bank/Account.java
@@ -1,22 +1,57 @@
 package uk.co.asepstrath.bank;
 
+import java.math.BigDecimal;
+import static uk.co.asepstrath.bank.Constants.MAX_BALANCE;
+
 public class Account {
-    int balance;
 
-    public Account(){
-        balance = 0;
+    private final String accountId;
+    private final String name;
+    private BigDecimal balance;
+
+    public Account(String accountId, String name, BigDecimal balance) {
+        this.accountId = accountId;
+        this.name = name;
+        this.balance = (balance != null) ? balance : BigDecimal.ZERO;
     }
 
-
-    public void deposit(int amount) {
-        if (amount <=0){
-            throw new IllegalArgumentException("Deposit amount must be positive");
+    public void deposit(BigDecimal amount) throws ArithmeticException {
+        if (amount == null) {
+            throw new ArithmeticException("Deposit amount cannot be null");
         }
-        balance += amount;
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new ArithmeticException("Deposit amount must be greater than 0");
+        }
+        if (balance.add(amount).compareTo(MAX_BALANCE) > 0) {
+            throw new ArithmeticException("Deposit would exceed maximum account balance");
+        }
+        balance = balance.add(amount);
     }
 
-    public int getBalance() {
-        return balance;
+    public void withdraw(BigDecimal amount) throws ArithmeticException {
+        if (amount == null) {
+            throw new ArithmeticException("Withdrawal amount cannot be null");
+        }
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new ArithmeticException("Withdrawal amount must be greater than 0");
+        }
+        if (amount.compareTo(balance) > 0) {
+            throw new ArithmeticException("Insufficient funds");
+        }
+        balance = balance.subtract(amount);
     }
 
+    public BigDecimal getBalance() { return balance; }
+    public String getAccountId() { return accountId; }
+    public String getName() { return name; }
+
+    public void updateBalance(BigDecimal newBalance) {
+        this.balance = newBalance;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Account{id='%s', name='%s', balance=%s}",
+                accountId, name, balance);
+    }
 }
