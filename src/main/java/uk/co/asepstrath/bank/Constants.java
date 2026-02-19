1a7b627969a89f701c3d289c7fdd3954bc40bc17
Alexander Marshall <alexander.marshall.2024@uni.strath.ac.uk>
2026-02-19 18:14:30 +0000

    Created constants class that will be used within the rest of the project

---
+++ src/main/java/uk/co/asepstrath/bank/Constants.java
@@ -0,0 +1,23 @@
+package uk.co.asepstrath.bank;
+
+import java.math.BigDecimal;
+
+public final class Constants {
+
+    public static final String ROUTE_ACCOUNT = "/account";
+    public static final String ROUTE_DEPOSIT = "/deposit";
+    public static final String ROUTE_WITHDRAW = "/withdraw";
+    public static final String ROUTE_PROCESS = "/process";
+
+    public static final String TEMPLATE_DEPOSIT = "deposit.hbs";
+    public static final String TEMPLATE_ACCOUNT = "account.hbs";
+
+    public static final String SESSION_ACCOUNT_ID = "accountid";
+    public static final String SESSION_ACCOUNT_NAME = "name";
+    public static final String SESSION_ERROR_MESSAGE = "error";
+    public static final String SESSION_SUCCESS_MESSAGE = "success";
+
+    public static final BigDecimal MAX_BALANCE = BigDecimal.valueOf(999_999_999.99);
+
+    private Constants() {}
+}
